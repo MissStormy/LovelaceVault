ca4775c5aee0e4e2dd30211b6faa149cf85a8e30
Lara RG <laragoblin@outlook.com>
2024-04-17 08:43:50 +0200

    fix: database covers

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -69,33 +69,33 @@ class DBHelper {
       await db.execute(
           "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(2, 'Horrorstör', 'Grady Hendrix', 'Libro', 4321, 'Lorem fugiat ad adipisicing ut commodo mollit nulla anim fugiat labore laborum ullamco.', 'assets/covers/horrorstor.jpg', 'FNAC')");
       await db.execute(
-          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(3, '1984', 'George Orwell', 'Libro', 1234, 'Novela distópica que presenta un futuro totalitario en el que el gobierno controla todos los aspectos de la vida de los ciudadanos.', '', 'Amazon')");
+          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(3, '1984', 'George Orwell', 'Libro', 1234, 'Novela distópica que presenta un futuro totalitario en el que el gobierno controla todos los aspectos de la vida de los ciudadanos.', 'assets/covers/book.jpeg', 'Amazon')");
 
       await db.execute(
-          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(4, 'La influencia de la inteligencia artificial en la sociedad contemporánea', 'María Pérez', 'Tesis', 5678, 'Estudio sobre el impacto de la inteligencia artificial en diferentes ámbitos sociales, como la educación, la medicina y el mercado laboral.', '', 'Repositorio universitario')");
+          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(4, 'La influencia de la inteligencia artificial en la sociedad contemporánea', 'María Pérez', 'Tesis', 5678, 'Estudio sobre el impacto de la inteligencia artificial en diferentes ámbitos sociales, como la educación, la medicina y el mercado laboral.', 'assets/covers/book.jpeg', 'Repositorio universitario')");
 
       await db.execute(
-          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(5, 'Guía práctica de programación en Python', 'Desconocido', 'Recurso en línea', 999, 'Manual de referencia para aprender los fundamentos de la programación en Python, con ejemplos prácticos y ejercicios.', '', 'Sitio web de programación')");
+          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(5, 'Guía práctica de programación en Python', 'Desconocido', 'Recurso en línea', 999, 'Manual de referencia para aprender los fundamentos de la programación en Python, con ejemplos prácticos y ejercicios.', 'assets/covers/book.jpeg', 'Sitio web de programación')");
 
       await db.execute(
-          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(6, 'El Principito', 'Antoine de Saint-Exupéry', 'Libro', 1357, 'Clásico de la literatura infantil que narra las aventuras de un joven príncipe en diferentes planetas, explorando temas como la amistad y la humanidad.', '', 'Casa del Libro')");
+          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(6, 'El Principito', 'Antoine de Saint-Exupéry', 'Libro', 1357, 'Clásico de la literatura infantil que narra las aventuras de un joven príncipe en diferentes planetas, explorando temas como la amistad y la humanidad.', 'assets/covers/book.jpeg', 'Casa del Libro')");
 
       await db.execute(
-          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(7, 'Impacto de la globalización en la economía mundial', 'Javier Rodríguez', 'Tesis', 2468, 'Análisis exhaustivo de los efectos de la globalización en los mercados internacionales, incluyendo aspectos económicos, sociales y culturales.', '', 'Repositorio universitario')");
+          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(7, 'Impacto de la globalización en la economía mundial', 'Javier Rodríguez', 'Tesis', 2468, 'Análisis exhaustivo de los efectos de la globalización en los mercados internacionales, incluyendo aspectos económicos, sociales y culturales.', 'assets/covers/book.jpeg', 'Repositorio universitario')");
       await db.execute(
-          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(8, 'El arte de la guerra', 'Sun Tzu', 'Libro', 2010, 'Tratado militar clásico que aborda estrategias y tácticas militares, aplicables también en el ámbito empresarial y de liderazgo.', '', 'Librería local')");
+          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(8, 'El arte de la guerra', 'Sun Tzu', 'Libro', 2010, 'Tratado militar clásico que aborda estrategias y tácticas militares, aplicables también en el ámbito empresarial y de liderazgo.', 'assets/covers/book.jpeg', 'Librería local')");
 
       await db.execute(
-          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(9, 'Inteligencia emocional', 'Daniel Goleman', 'Libro', 1850, 'Exploración sobre la importancia de la inteligencia emocional en el éxito personal y profesional, con estrategias para su desarrollo.', '', 'Biblioteca pública')");
+          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(9, 'Inteligencia emocional', 'Daniel Goleman', 'Libro', 1850, 'Exploración sobre la importancia de la inteligencia emocional en el éxito personal y profesional, con estrategias para su desarrollo.', 'assets/covers/book.jpeg', 'Biblioteca pública')");
 
       await db.execute(
-          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(10, 'El origen de las especies', 'Charles Darwin', 'Libro', 3142, 'Obra seminal que introduce la teoría de la evolución mediante la selección natural, revolucionando la comprensión de la biología y la historia natural.', '', 'Tienda de libros usados')");
+          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(10, 'El origen de las especies', 'Charles Darwin', 'Libro', 3142, 'Obra seminal que introduce la teoría de la evolución mediante la selección natural, revolucionando la comprensión de la biología y la historia natural.', 'assets/covers/book.jpeg', 'Tienda de libros usados')");
 
       await db.execute(
-          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(11, 'Cómo ganar amigos e influir sobre las personas', 'Dale Carnegie', 'Libro', 2756, 'Guía clásica sobre habilidades sociales y técnicas de persuasión, con principios prácticos para mejorar las relaciones interpersonales y la comunicación.', '', 'Librería online')");
+          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(11, 'Cómo ganar amigos e influir sobre las personas', 'Dale Carnegie', 'Libro', 2756, 'Guía clásica sobre habilidades sociales y técnicas de persuasión, con principios prácticos para mejorar las relaciones interpersonales y la comunicación.', 'assets/covers/book.jpeg', 'Librería online')");
 
       await db.execute(
-          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(12, 'La teoría de la relatividad', 'Albert Einstein', 'Libro', 1875, 'Explicación accesible de la teoría de la relatividad de Einstein, que transformó nuestra comprensión del tiempo, el espacio y la gravedad.', '', 'Universidad')");
+          "INSERT INTO resources(id, title, author, type, bytes, summary, imagePath, location) VALUES(12, 'La teoría de la relatividad', 'Albert Einstein', 'Libro', 1875, 'Explicación accesible de la teoría de la relatividad de Einstein, que transformó nuestra comprensión del tiempo, el espacio y la gravedad.', 'assets/covers/book.jpeg', 'Universidad')");
 
       //TABLA DE USUARIOS REGISTRADOS EN LA APP
       await db.execute(
